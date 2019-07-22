860a7edfd5411b8bf4bb7f22aa0bf6c415cd24d9
yunseong <yskimno1@gmail.com>
2019-07-22 15:53:55 +0900

    new request generator

---
+++ input_generator.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='generate requests.')
+parser.add_argument('number', type=int, help='number of requests')
+parser.add_argument('interval', type=float, help='interval between requests')
+
+args = parser.parse_args()
+# print(args.number)
+
+f = open('requests.txt', 'w')
+for i in range(args.number):
+  req = 1 + args.interval * i
+  if(i == args.number-1):
+      data = str(req)
+  else:
+      data = str(req) + ","
+  f.write(data)
+
+
+f.close()
