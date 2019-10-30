28b73d900145cff4886c48bfd2cfb9ef5c27c5b9
yunseong <yskimno1@gmail.com>
2019-10-30 17:16:38 +0900

    changed env

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
 #/usr/bin/bash
-/usr/local/cuda-10.1/bin/nvcc -c $HOME/Project/model.cu  -lcudnn -lcublas -lz -std=c++11
-/usr/local/cuda-10.1/bin/nvcc -c $HOME/Project/layer.cu  -lcudnn -lcublas -lz -std=c++11
-/usr/local/cuda-10.1/bin/nvcc -o $HOME/Project/anybatch $HOME/Project/cudnn.cu $HOME/Project/build/layer.o $HOME/Project/build/model.o -lcudnn -lcublas -lz -std=c++11
+/usr/local/cuda-10.1/bin/nvcc -c $HOME/new/Project/model.cu  -lcudnn -lcublas -lz -std=c++11
+/usr/local/cuda-10.1/bin/nvcc -c $HOME/new/Project/layer.cu  -lcudnn -lcublas -lz -std=c++11
+/usr/local/cuda-10.1/bin/nvcc -o $HOME/new/Project/anybatch $HOME/new/Project/cudnn.cu $HOME/new/Project/build/layer.o $HOME/new/Project/build/model.o -lcudnn -lcublas -lz -std=c++11
